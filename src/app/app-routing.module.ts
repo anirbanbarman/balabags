a2c3534225dda24e0926c418d9075ae521cdf080
anirbanbarman <anirbandali247@gmail.com>
2022-05-17 17:17:46 +0530

    about page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,7 +3,8 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 
-const routes: Routes = [{ path: 'home', loadChildren: () => import('./pages/home/home.module').then(m => m.HomeModule) },
+const routes: Routes = [
+  { path: 'home', loadChildren: () => import('./pages/home/home.module').then(m => m.HomeModule) },
 
 { path: 'about-us', loadChildren: () => import('./pages/about-us/about-us.module').then(m => m.AboutUsModule) },
 
--- src/app/pages/about-us/about-us-routing.module.ts
+++ src/app/pages/about-us/about-us-routing.module.ts
@@ -2,7 +2,8 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AboutUsComponent } from './about-us.component';
 
-const routes: Routes = [{ path: '', component: AboutUsComponent }];
+const routes: Routes = [
+  { path: '', component: AboutUsComponent }];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
