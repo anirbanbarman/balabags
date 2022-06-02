cf9eaf7cd72ec2e442515463fdd35d15d689a314
AVISHEK DAS <avishekdas478@gmail.com>
2022-06-03 01:21:28 +0530

    Home page in progress

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { OwlOptions } from 'ngx-owl-carousel-o';
 
 @Component({
   selector: 'app-home',
@@ -12,4 +13,26 @@ export class HomeComponent implements OnInit {
   ngOnInit(): void {
   }
 
+  customOptions: OwlOptions = {
+    loop: true,
+    dots: true,
+    nav:false,
+    autoplay:false,
+    margin:15,
+    // navText: ['left', 'right'],
+    responsive: {
+      0: {
+        items: 1
+      },
+      400: {
+        items: 1
+      },
+      740: {
+        items: 2
+      },
+      940: {
+        items: 3
+      }
+    }
+  }
 }
--- src/app/pages/home/home.module.ts
+++ src/app/pages/home/home.module.ts
@@ -3,15 +3,16 @@ import { CommonModule } from '@angular/common';
 
 import { HomeRoutingModule } from './home-routing.module';
 import { HomeComponent } from './home.component';
-
+import { CarouselModule } from 'ngx-owl-carousel-o';
 
 @NgModule({
   declarations: [
-    HomeComponent
+    HomeComponent,
   ],
   imports: [
     CommonModule,
-    HomeRoutingModule
+    HomeRoutingModule,
+    CarouselModule
   ]
 })
 export class HomeModule { }
